dad318ea70993d772c0664e88824f6672abd159f
jabir20102 <32677877+jabir20102@users.noreply.github.com>
2022-02-15 10:22:01 +0500

    chatt improved

--- resources/views/frontend/chat.blade.php
+++ resources/views/frontend/chat.blade.php
@@ -2,17 +2,37 @@
 
 @section('main')
     
-    <div class="row">
-        <div class="col-md-5">
+    
             @if($users->count() > 0)
              
-<h3>Messaging</h3>
-<div class="messaging">
+<div class="container-fluid">
+  <div>
+  <h3 class=" text-left">Chat  <span id="message_with"></span> </h3>
+  <p style="cursor: pointer" id="clear-btn" class=" text-danger">Clear chat history</p>
+          
+  <div class="messaging">
       <div class="inbox_msg">
+        <div class="mesgs">
+           
+          <div id="chat_history" class="msg_history">
+                  
+          </div>
+          <div class="type_msg">
+            <div class="input_msg_write">
+                <form id="sendForm" method="POST">
+                    @csrf
+              <input id="message" name="message" type="text" class="write_msg" placeholder="Type a message"  />
+              <button id="btn-submit" class="msg_send_btn" type="submit">
+                  <i class="fa fa-paper-plane-o" aria-hidden="true"></i>
+              </button>
+          </form>
+            </div>
+          </div>
+        </div>
         <div class="inbox_people">
           <div class="inbox_chat">
             @foreach($users as $user)
-                <div class="chat_list active_chat">
+                <div id="active_add_{{$user->id}}" class="chat_list ">
                 <div class="chat_people">
                     <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
                     <div class="chat_ib">
@@ -28,71 +48,20 @@
                 @endforeach
           </div>
         </div>
-      </div>
-</div>      
-
-
+      
+  
+        
+       
+  
+       </div>
+  </div>
+</div>
 
             @else
                 <p>No users found! try to add a new user using another browser by going to <a href="{{ url('register') }}">Register page</a></p>
             @endif
-        </div>
-    </div>
-    
-<!-- Modal -->
-<div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog">
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <button onclick="cancel()" type="button" class="close" data-dismiss="modal">&times;</button>
-          
-          <h4 class="modal-title">Chat with <span id="message_with"></span> 
-            <button style="float: left" id="clear-btn" class="btn btn-danger btn-sm">Clear chat history</button>
-          
-          </h4>
-          
-        </div>
-        <div class="modal-body">
-         
-          <div class="messaging">
-              <div class="inbox_msg">
-                     
-        
-                <div class="mesgs">
-                 
-                  <div id="chat_history" class="msg_history">
-                          
-                  </div>
-                  <div class="type_msg">
-                    <div class="input_msg_write">
-                        <form id="sendForm" method="POST">
-                            @csrf
-                      <input id="message" name="message" type="text" class="write_msg" placeholder="Type a message" />
-                      <button id="btn-submit" class="msg_send_btn" type="submit">
-                          <i class="fa fa-paper-plane-o" aria-hidden="true"></i>
-                      </button>
-                  </form>
-                    </div>
-                  </div>
-                </div>
-        
-              </div>
-              
-             
-        </div>
-        </div>
-        
-      </div>
-    </div>
-  </div>
-  
 
- 
-
-
-
-    @section('script')
+            @section('script')
     <script type="text/javascript">  
         var to="";
        // Enable pusher logging - don't include this in production
@@ -136,7 +105,8 @@
            
            
            to=$(this).attr('id');
-           
+           $('active_add_'+$(this).attr('id')). addClass('active_chat');
+          //  alert($('active_add_'+$(this).attr('id').id));
            document.getElementById('message_with').innerHTML=$(this).html();//attr('id');
            $.ajax({    
                type:'GET',    
@@ -153,7 +123,8 @@
        $("#btn-submit").click(function(e){
            
            e.preventDefault();
-           
+           if(to!=""){
+             if($('#message').val()!=""){
            $.ajax({    
                type:'POST',    
                url:"{{ url('sendMessage') }}",    
@@ -169,7 +140,11 @@
                      $('#chat_history').scrollTop($('#chat_history')[0].scrollHeight); 
                      $('#message').val('');
                }
-           }); 
+           });
+             } 
+           }else{
+             alert("Select a person to chat");
+           }
        });
        $("#clear-btn").click(function(e){
            
